fb1c99eb9dfd64731e885e2e52f5341d94f22d0e
Michael van der Weg <michael@joinbox.com>
2018-08-29 23:05:56 +0200

    style: rename variable

--- functions/infect.reducer.mjs
+++ functions/infect.reducer.mjs
@@ -96,16 +96,16 @@ class Reducer {
 
         // do some shiny corrections, agresti-coull:
         // https://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval
-        const someVariable = (resistantCount + 2) / (sampleCount + 4);
+        const proportionResistant = (resistantCount + 2) / (sampleCount + 4);
 
         // standard error value
-        const stdandardError = Math.sqrt(someVariable * (1 - someVariable) / (sampleCount + 4));
+        const stdandardError = Math.sqrt(proportionResistant * (1 - proportionResistant) / (sampleCount + 4));
 
         // lower confidence interval
-        const lowerCI = Math.max(Math.round(((someVariable - 1.96 * stdandardError) * 100), 1), 0);
+        const lowerCI = Math.max(Math.round(((proportionResistant - 1.96 * stdandardError) * 100), 1), 0);
 
         // upper confidence interval
-        const upperCI = Math.min(Math.round(((someVariable + 1.96 * stdandardError) * 100), 1), 100);
+        const upperCI = Math.min(Math.round(((proportionResistant + 1.96 * stdandardError) * 100), 1), 100);
 
         return {
             confidenceIntervalLowerBound: lowerCI,
